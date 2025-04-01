890071e6a70a00b457e98454b3f4b8e7476c2c83
dianamocanu97 <diana.mocanu97@outlook.com>
2025-04-01 18:30:42 +0200

    Update Table 2.m

--- Table 2.m
+++ Table 2.m
@@ -1,24 +1,27 @@
 //Verifying assumptions of Table 2
 
-P<x> := PolynomialRing(Rationals());
+Q2 := pAdicField(2, 20);
+P<x> := PolynomialRing(Q2);
 
 // Examples with e=8
 examples := [
-    <x^8 + 2*x^6 + 4*x^3 + 4*x + 2, "96a1">,
-    <x^8 + 2*x^6 + 4*x^3 + 4*x + 2, "2592f1">,
-    <x^8 + 2*x^6 + 4*x^3 + 4*x + 6, "288a1">,
-    <x^8 + 2*x^6 + 4*x^3 + 4*x + 6, "288e2">,
-    <x^8 + 8*x^7 + 12*x^6 + 14*x^4 + 4*x^2 + 8*x + 14, "256b2">,
-    <x^8 + 8*x^7 + 12*x^6 + 14*x^4 + 4*x^2 + 8*x + 14, "2304a2">
+    <x^8 + 2*x^6 + 4*x^3 + 4*x + 2, [0, 1, 0, -2, 0]>,
+    <x^8 + 2*x^6 + 4*x^3 + 4*x + 2, [0, 0, 0, -108, -864]>,
+    <x^8 + 2*x^6 + 4*x^3 + 4*x + 6, [0, 0, 0, 3, 0]>,
+    <x^8 + 2*x^6 + 4*x^3 + 4*x + 6, [0, 0, 0, -108, 0]>,
+    <x^8 + 8*x^7 + 12*x^6 + 14*x^4 + 4*x^2 + 8*x + 14, [0, 0, 0, 8, 0]>,
+    <x^8 + 8*x^7 + 12*x^6 + 14*x^4 + 4*x^2 + 8*x + 14, [0, 0, 0, 216, 0]>
 ];
 
 for  ex in examples do
-    print "Example label:", ex[2];
-    f:=ex[1]; F := NumberField(f);
-    E := EllipticCurve(ex[2]); E := MinimalModel(E);
-    t:=LocalInformation(E,2);
+    L := ext<Q2 |  ex[1]>;
+    E := EllipticCurve( ex[2]);
+    printf "Cremona label is %o\n", CremonaReference(E);
     
-    print "Conductor exponent at 2 = ",t[3] ;
+    E := MinimalModel(E);
+    E := BaseChange(E, Q2);
+    
+    print "Conductor of E is", Conductor(E);
     
     // Compute invariants
     c4, c6 := Explode(cInvariants(E));
@@ -36,33 +39,33 @@ for  ex in examples do
     tc4 := c4 div 2^v4;
     print "tilde{c_4} =", tc4 mod 4, "mod 4";
     
-    //Check good reduction at L'
-    E := BaseChange(E, F);
-    OF:=MaximalOrder(F);
-    P:=Factorization(2*OF);
-    t:=LocalInformation(E,P[1][1]);
-    if t[3] eq 0 then
-        print "E has good reduction over the extension of Q_2 by",f;
+    // Check good reduction over L'
+    E := BaseChange(E, L);
+    if Integers()!Conductor(E) eq 1 then
+        print "E has good reduction over", L;
     end if;
-        print "********************";
+    print "********************";
 end for;
 
 // Examples with e=24
 
 examples := [
-    <x^8 + 2*x^3 + 2*x^2 + 2, "648b1">,
-    <x^8 + 2*x^3 + 2*x^2 + 2, "6696q1">,
-    <x^8 + 4*x^7 + 8*x^4 + 8*x^3 + 4*x^2 + 8*x + 10, "3456a1">,
-    <x^8 + 4*x^7 + 8*x^4 + 8*x^3 + 4*x^2 + 8*x + 10, "289152l1">
+    <x^8 + 2*x^3 + 2*x^2 + 2, [0, 0, 0, -3, -1]>,
+    <x^8 + 2*x^3 + 2*x^2 + 2, [0, 0, 0, -135, -54]>,
+    <x^8 + 4*x^7 + 8*x^4 + 8*x^3 + 4*x^2 + 8*x + 10, [0, 0, 0, 3, 2]>,
+    <x^8 + 4*x^7 + 8*x^4 + 8*x^3 + 4*x^2 + 8*x + 10, [0, 0, 0, 1080, -864]>
 ];
 
 for ex in examples do
-    print "Example label:", ex[2];
-    f := ex[1]; F := SplittingField(f);
-    E := EllipticCurve(ex[2]); E := MinimalModel(E);
-    t:=LocalInformation(E,2);
+    f := ex[1];
+    F := SplittingField(f);
+    E := EllipticCurve(ex[2]);
+    printf "Cremona label is %o\n", CremonaReference(E);
     
-    print "Conductor exponent at 2 = ",t[3] ;
+    E := MinimalModel(E);
+    E := BaseChange(E, Q2);
+    
+    print "Conductor of E is", Conductor(E);
     
     // Compute invariants
     c4, c6 := Explode(cInvariants(E));
@@ -78,14 +81,10 @@ for ex in examples do
     
     print "v_2(Delta_m) = ", vD mod 3, "mod 3";
     
-//Check good reduction at L'
+    // Check good reduction over F'
     E := BaseChange(E, F);
-    OF:=MaximalOrder(F);
-    P:=Factorization(2*OF);
-    t:=LocalInformation(E,P[1][1]);
-    if t[3] eq 0 then
-        print "E has good reduction over the splitting field of",f, "over Q_2";
+    if Integers()!Conductor(E) eq 1 then
+        print "E has good reduction over the splitting field of", f;
     end if;
-        print "********************";
-end for;
+    print "********************";
 end for;
